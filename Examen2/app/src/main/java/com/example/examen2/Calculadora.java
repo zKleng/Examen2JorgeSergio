6f76f40447f60ec089c770bdf4e5d59fd721b4df
zKleng <j2005mn@gmail.com>
2024-11-09 16:49:05 -0600

    V1.2.3
    Add news features from Calculator

--- Examen2/app/src/main/java/com/example/examen2/Calculadora.java
+++ Examen2/app/src/main/java/com/example/examen2/Calculadora.java
@@ -1,4 +1,7 @@
 package com.example.examen2;
+
+import java.util.List;
+
 public class Calculadora {
 
     // Método para convertir el número a la base seleccionada (0: Binario, 1: Octal, 2: Decimal, 3: Hexadecimal)
@@ -67,5 +70,8 @@ public class Calculadora {
             default:
                 throw new IllegalArgumentException("Base de destino no soportada");
         }
+    }
+    static boolean Encuentra(List<Integer> lista, int elemento){
+        return lista.contains(elemento);
     }
 }
